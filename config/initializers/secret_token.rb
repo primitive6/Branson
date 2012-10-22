2aa5ec2c42c3b715f92e5cb6c9a48558a79449e5
primitive6 <tony.staunton@outlook.com>
2012-10-22 09:58:35 +0100

    Branson CH3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Branson::Application.config.secret_token = '75404c9ce9a3a497da4d2d79bd28b1e696f5d62c3ec1b4028ad507392581b936fcd2092baa3e8360d75a4b8c6034bb638647453b9e459dfb4ba155686113686c'
